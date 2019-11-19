059a6c7a96c8cd3d68677ff82b555e5f546230ab
canewman <aaron.newman1111@gmail.com>
2019-11-18 19:01:55 -0600

    attempt at converting to longs. Errors somewhere at weekend change

--- src/edu/jsu/mcis/tas_fa19/Punch.java
+++ src/edu/jsu/mcis/tas_fa19/Punch.java
@@ -13,6 +13,7 @@ public class Punch {
     private int terminalid;
     private String badgeid;
     private GregorianCalendar adjustedtimestamp = new GregorianCalendar();
+    private long adjustedTimeStampInMill;
     private String adjustmenttype;
     private long originalTimeStamp;
     private GregorianCalendar gc = new GregorianCalendar();
@@ -39,6 +40,7 @@ public class Punch {
     public void setOriginalTimestamp(long ts)//a punch pulled from sql
     {
        gc.setTimeInMillis(ts);
+       this.originalTimeStamp = ts;
     }
 
     public String printOriginalTimestamp() {
@@ -105,76 +107,146 @@ public class Punch {
     }
     
     public void adjust(Shift s){
+            //System.out.println(adjustedtimestamp.getTimeInMillis());
+            //LocalTime gcLocalTime = LocalTime.of((int)gc.get(Calendar.HOUR_OF_DAY), (int)gc.get(Calendar.MINUTE));
+            adjustedtimestamp.setTimeInMillis(gc.getTimeInMillis());            
+            adjustmenttype = "";
+            //            System.out.println(adjustedtimestamp.getTimeInMillis());
 
-            LocalTime gcLocalTime = LocalTime.of((int)gc.get(Calendar.HOUR_OF_DAY), (int)gc.get(Calendar.MINUTE));
-            adjustedtimestamp.setTimeInMillis(gc.getTimeInMillis());
             int dayOfWeek = adjustedtimestamp.get(Calendar.DAY_OF_WEEK);
-            adjustmenttype = "None";
+            int shiftinterval = s.getInterval();//in minutes
+
+            //getting start of shift parameters in milliseconds
+            int startHour = s.getStart().getHour();
+            int startMinute = s.getStart().getMinute();
+            long interval = s.getInterval() * 60000;
+            long grace = s.getGracePeriod() * 60000;
+            long dock = s.getDock() * 60000;
             
-            int shiftinterval = s.getInterval();
+            
+            //shift start
+            GregorianCalendar start = new GregorianCalendar();
+            start.setTimeInMillis(gc.getTimeInMillis());
+            start.set(Calendar.HOUR_OF_DAY, startHour);
+            start.set(Calendar.MINUTE, startMinute);
+            start.set(Calendar.SECOND, 0);
+                   
+            //System.out.println(start.getTimeInMillis());
+            
+            long shiftStart = start.getTimeInMillis();
+            long startInterval = shiftStart - interval;//parentheses converts the 15 minute interval to mill
+            long startGrace = shiftStart + grace;//parentheses converts the 5 minute grace period to mill
+            long startDock = shiftStart + dock;//parentheses converts the 15 minute dock to mill
+            
+            //lunch start
+            GregorianCalendar lunchstart = new GregorianCalendar();
+            lunchstart.setTimeInMillis(shiftStart);//sets lunch to the punch day
+            lunchstart.set(Calendar.HOUR, s.getLunchStart().getHour());
+            lunchstart.set(Calendar.MINUTE, s.getLunchStart().getMinute());
+            lunchstart.set(Calendar.SECOND, 0);
+            
+            long lunchStart = lunchstart.getTimeInMillis();
+            
+            //lunch stop
+            GregorianCalendar lunchstop = new GregorianCalendar();
+            lunchstop.setTimeInMillis(shiftStart);//sets lunch to the punch day
+            lunchstop.set(Calendar.HOUR, s.getLunchStop().getHour());
+            lunchstop.set(Calendar.MINUTE, s.getLunchStop().getMinute());
+            lunchstop.set(Calendar.SECOND, 0);
+            
+            long lunchStop = lunchstop.getTimeInMillis();
+            
+            //getting end of shift parameters in mill
+            int stopHour = s.getStop().getHour();
+            int stopMinute = s.getStop().getMinute();
+            
+            //Shift stop
+            GregorianCalendar stop = new GregorianCalendar();
+            stop.setTimeInMillis(shiftStart);//set stop shift to punch day
+            stop.set(Calendar.HOUR_OF_DAY, stopHour);
+            stop.set(Calendar.MINUTE, stopMinute);
+            stop.set(Calendar.SECOND, 0);              
+            
+            long shiftStop = stop.getTimeInMillis();
+            long stopInterval = shiftStop + interval;
+            long stopGrace = shiftStop - grace;
+            long stopDock = shiftStop - dock;
+            
+            switch(s.getID()){
+            
+                case 0://clock out
+                    if((originalTimeStamp >= lunchStart) && (originalTimeStamp <= lunchStop)){//start of lunch
+                        this.adjustmenttype = "Lunch Start";
+                        this.adjustedTimeStampInMill = lunchStart;
+                    //    System.out.println(lunchStart);
+                    }
+                    else if((originalTimeStamp < stopGrace) && (originalTimeStamp >= stopDock)){//stop dock
+                        this.adjustmenttype = "Shift Dock";
+                        this.adjustedTimeStampInMill = stopDock;
+                    //                            System.out.println(stopDock);
 
-            LocalTime startGraceTime = s.getStart().plusMinutes(s.getGracePeriod());
-            LocalTime startIntervalTime = s.getStart().minusMinutes(s.getInterval());
-            LocalTime startDockTime = s.getStart().plusMinutes(s.getDock());
+                    }
+                    else if((originalTimeStamp >= stopGrace) && (originalTimeStamp <= shiftStop)){//stop grace
+                        this.adjustmenttype = "Shift Stop";
+                        this.adjustedTimeStampInMill = shiftStop;   
+                    //                            System.out.println(stopGrace);
 
-            LocalTime stopGraceTime = s.getStop().plusMinutes(s.getGracePeriod());
-            LocalTime stopIntervalTime = s.getStop().plusMinutes(s.getInterval());
-            LocalTime stopDockTime = s.getStop().plusMinutes(s.getDock());
+                    }
+                    else if((originalTimeStamp >= shiftStop) && (originalTimeStamp <= stopInterval)){//stop interval
+                        this.adjustmenttype = "Shift Stop";
+                        this.adjustedTimeStampInMill = shiftStop;
+                    //                            System.out.println(stopInterval);
 
-            LocalTime lunchStart = s.getLunchStart();
-            LocalTime lunchStop = s.getLunchStop();
-           
-            if(gcLocalTime.isAfter(startIntervalTime) && gcLocalTime.isBefore(s.getStart())){//start interval
-            gcLocalTime = s.getStart();
-            this.adjustmenttype = "Shift Start";
+                    }  
+                    //System.out.println("case 1 " + adjustedTimeStampInMill);
+                    this.adjustedtimestamp.setTimeInMillis(adjustedTimeStampInMill);
+                    break;
+                case 1://clock in
+                    
+                    if((originalTimeStamp >= startInterval) && (originalTimeStamp <= shiftStart)){//start interval
+                        this.adjustmenttype = "Shift Start";
+                        this.adjustedTimeStampInMill = shiftStart;   
+                        System.out.println("Start Interval");
+                    }
+                    else if((originalTimeStamp >= shiftStart) && (originalTimeStamp <= startGrace)){////start grace
+                        this.adjustmenttype = "Shift Start";
+                        this.adjustedTimeStampInMill = shiftStart;
+                        System.out.println("start grace");
+                    }
+                    else if((originalTimeStamp > startGrace) && (originalTimeStamp <= startDock)){//start dock
+                        this.adjustmenttype = "Shift Dock";
+                        this.adjustedTimeStampInMill = startDock;
+                        System.out.println("Shift Dock");
+                    }
+                    else if((originalTimeStamp >= lunchStart) && (originalTimeStamp <= lunchStop)){//start lunch
+                        this.adjustmenttype = "Lunch Stop";
+                        this.adjustedTimeStampInMill = lunchStop;
+                        System.out.println("lunch stop");
+                        System.out.println(lunchStop);
+                    }
+                    
+                    //System.out.println("case 2" + adjustedTimeStampInMill);
+                    this.adjustedtimestamp.setTimeInMillis(adjustedTimeStampInMill);
+                    break;
             }
-            else if(dayOfWeek != Calendar.SATURDAY && dayOfWeek != Calendar.SUNDAY && gcLocalTime.isAfter(s.getStart()) && gcLocalTime.isBefore(startGraceTime)){//start grace period
-            gcLocalTime = s.getStart();
-            this.adjustmenttype = "Shift Start";
-            }
-            else if(dayOfWeek != Calendar.SATURDAY && dayOfWeek != Calendar.SUNDAY && gcLocalTime.isAfter(startGraceTime) && gcLocalTime.isBefore(startDockTime)){//start dock period
-            gcLocalTime = startDockTime;
-            this.adjustmenttype = "Shift Dock";
-            }
-            else if(dayOfWeek != Calendar.SATURDAY && dayOfWeek != Calendar.SUNDAY && punchtypeid == 0 && gcLocalTime.isAfter(lunchStart) && gcLocalTime.isBefore(lunchStop)){//start lunch
-            gcLocalTime = s.getLunchStart();
-            this.adjustmenttype = "Lunch Start";
-            }
-            else if(dayOfWeek != Calendar.SATURDAY && dayOfWeek != Calendar.SUNDAY && punchtypeid == 1 && gcLocalTime.isAfter(lunchStart) && gcLocalTime.isBefore(lunchStop)){//stop lunch
-            gcLocalTime = s.getLunchStop();
-            this.adjustmenttype = "Lunch Stop";
-            }
-            else if(dayOfWeek != Calendar.SATURDAY && dayOfWeek != Calendar.SUNDAY && gcLocalTime.isAfter(stopDockTime) && gcLocalTime.isBefore(stopGraceTime)){//stop dock period
-            gcLocalTime = stopDockTime;
-            this.adjustmenttype = "Shift Dock";
-            }
-            else if(dayOfWeek != Calendar.SATURDAY && dayOfWeek != Calendar.SUNDAY && gcLocalTime.isAfter(stopGraceTime) && gcLocalTime.isBefore(s.getStop())){//stop grace period
-            gcLocalTime = s.getStop();
-            this.adjustmenttype = "Shift Stop";
-            }
-            else if(gcLocalTime.isAfter(s.getStop()) && gcLocalTime.isBefore(stopIntervalTime)){//stop interval period
-            gcLocalTime = s.getStop();
-            this.adjustmenttype = "Shift Stop";
-            }
-            adjustedtimestamp.set(Calendar.HOUR_OF_DAY, gcLocalTime.get(ChronoField.HOUR_OF_DAY));
-            adjustedtimestamp.set(Calendar.MINUTE, gcLocalTime.get(ChronoField.MINUTE_OF_HOUR));
-            adjustedtimestamp.set(Calendar.SECOND, gcLocalTime.get(ChronoField.SECOND_OF_MINUTE));
             
             if(dayOfWeek == Calendar.SUNDAY || dayOfWeek == Calendar.SATURDAY){//rounding up
                 if(gc.get(Calendar.MINUTE) % s.getInterval() >= s.getInterval() / 2){
                     
-                    while(adjustedtimestamp.get(Calendar.MINUTE) != 0){
-                        adjustedtimestamp.add(Calendar.MINUTE, 1);
-                    }           
-                    
+                    adjustedtimestamp.set(Calendar.MINUTE, gc.get(Calendar.MINUTE) + (gc.get(Calendar.MINUTE) % s.getInterval()));                    
                     adjustedtimestamp.set(Calendar.SECOND, 0);
+                    System.out.println(adjustedtimestamp.getTimeInMillis());
+                    System.out.println("Rounding up");
                 }
                 else{//rounding down               
                 adjustedtimestamp.set(Calendar.MINUTE, adjustedtimestamp.get(Calendar.MINUTE) - gc.get(Calendar.MINUTE) % s.getInterval());
                 adjustedtimestamp.set(Calendar.SECOND, 0);
+                System.out.println(adjustedtimestamp.getTimeInMillis());
+                System.out.println("rounding down");
                 }
                 
                 this.adjustmenttype = "Interval Round";             
+                System.out.println("entered my weekend code");
                 
             }
           
@@ -183,16 +255,21 @@ public class Punch {
             int adjustedminute;
             
             if(originalminute % shiftinterval != 0){
-                if((originalminute % shiftinterval) < (shiftinterval / 2))
+                if((originalminute % shiftinterval) < (shiftinterval / 2)){//rounding down
                     adjustedminute = (Math.round(originalminute / shiftinterval) * shiftinterval);
-                else{
+                    System.out.println("rounding down");
+                }
+                else{//rounding up
                     adjustedminute = (Math.round(originalminute / shiftinterval) * shiftinterval) + shiftinterval;
+                    System.out.println("rounding up");
                 }
                 adjustedtimestamp.set(Calendar.MINUTE, (adjustedminute - originalminute));
                 adjustedtimestamp.set(Calendar.SECOND, 00);
+                adjustmenttype = "Interval Round";
+                System.out.println("Entered snellens code");
                 
             }            
-
+            
             
             
            
