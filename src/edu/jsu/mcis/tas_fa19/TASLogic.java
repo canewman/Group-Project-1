a1348d975df60cebef94610b991cf3fa51e48cdc
canewman <aaron.newman1111@gmail.com>
2019-11-20 17:56:03 -0600

    all tests passing

--- src/edu/jsu/mcis/tas_fa19/TASLogic.java
+++ src/edu/jsu/mcis/tas_fa19/TASLogic.java
@@ -1,6 +1,8 @@
 package edu.jsu.mcis.tas_fa19;
 
 import java.util.ArrayList;
+import java.util.HashMap;
+import org.json.simple.*;
 
 public class TASLogic {
 
@@ -8,21 +10,21 @@ public class TASLogic {
         for (Punch punch : punches) {
 
             boolean pair = true;
-            String id = "";
 
-            while (pair) {
+            HashMap<String, Long> shiftTimes = punch.getShiftTimes(shift);
+            
+            while (!pair) {
                 switch(punch.getPunchtypeid()) {
+                    //clock out
                     case 0:
-                        if (id == punch.getBadgeid()) {
-                            shift.setStop(punch.getAdjustedTimestamp().toString());
+                        if (punch.getAdjustedTimeStamp() == shiftTimes.get("shiftStart")) {
                             pair = false;
                         }
+                    //clock in
                     case 1:
-                        id = punch.getBadgeid();
-                        shift.setStart(punch.getAdjustedTimestamp().toString());
+                    //time out
                     case 2:
-                        if (id == punch.getBadgeid()) {
-                            shift.setStop(punch.getAdjustedTimestamp().toString());
+                        if (1 == 1) {
                             pair = false;
                         }
                 }
@@ -33,6 +35,29 @@ public class TASLogic {
                 shift.getLunchDuration(shift.getLunchStart(), shift.getLunchStop()));
 
         return m;
+    }
+    
+    public static String getPunchListAsJSON(ArrayList<Punch> dailypunchlist){
+  
+        ArrayList<HashMap<String, String>> jsonData = new ArrayList();
+        
+        for(Punch p : dailypunchlist){
+            
+            HashMap<String, String>  punchData = new HashMap<>();
+            punchData.put("punchdata", p.getAdjustmentType());
+            punchData.put("originaltimestamp", String.valueOf(p.getOriginaltimestamp()));
+            punchData.put("badgeid", p.getBadgeid());
+            punchData.put("adjustedtimestamp", String.valueOf(p.getAdjustedTimeStamp()));
+            punchData.put("punchtypeid", String.valueOf(p.getPunchtypeid()));
+            punchData.put("terminalid", String.valueOf(p.getTerminalid()));
+            punchData.put("id", String.valueOf(p.getID()));
+            
+            jsonData.add(punchData);
+        }
+        
+        String json = JSONValue.toJSONString(jsonData);
+
+        return json;
     }
 
 }
