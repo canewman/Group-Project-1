2116d7a451dc5962af7e893df061e15c17794246
colemantamason <54876724+colemantamason@users.noreply.github.com>
2019-11-22 12:05:31 -0600

    Heavily edited and non-complete Feature 4

--- src/edu/jsu/mcis/tas_fa19/TASLogic.java
+++ src/edu/jsu/mcis/tas_fa19/TASLogic.java
@@ -1,36 +1,104 @@
 package edu.jsu.mcis.tas_fa19;
 
+import java.time.Duration;
+import java.time.LocalTime;
 import java.util.ArrayList;
+import java.util.HashMap;
 
 public class TASLogic {
 
-    public static int calculateTotalMinutes(ArrayList<Punch> punches, Shift shift) {
+    /*public static int calculateTotalMinutes(ArrayList<Punch> punches, Shift shift) {
+
+        boolean pair = false;
+        long shiftStart = 0;
+        long shiftStop = 0;
+        long lunchStart = 0;
+        long lunchStop = 0;
+
         for (Punch punch : punches) {
 
-            boolean pair = true;
-            String id = "";
+            HashMap<String, Long> shiftTimes = punch.getShiftTimes(shift);
 
-            while (pair) {
+            while (!pair) {
                 switch(punch.getPunchtypeid()) {
+                    // Clock-Out
                     case 0:
-                        if (id == punch.getBadgeid()) {
-                            shift.setStop(punch.getAdjustedTimestamp().toString());
-                            pair = false;
+                        if (punch.getAdjustedTimeStamp() == shiftTimes.get("shiftStop")) {
+                            shiftStop = punch.getAdjustedTimeStamp();
+                            //Gives Minutes
+                            shiftStop = (shiftStop / 1000) * 60;
                         }
+                        else if (punch.getAdjustedTimeStamp() == shiftTimes.get("lunchStart")) {
+                            lunchStart = punch.getAdjustedTimeStamp();
+                            //Gives Minutes
+                            lunchStart = (lunchStart / 1000) * 60;
+                        }
+                        pair = true;
+                    // Clock-In
                     case 1:
-                        id = punch.getBadgeid();
-                        shift.setStart(punch.getAdjustedTimestamp().toString());
-                    case 2:
-                        if (id == punch.getBadgeid()) {
-                            shift.setStop(punch.getAdjustedTimestamp().toString());
-                            pair = false;
+                        if (punch.getAdjustedTimeStamp() == shiftTimes.get("shiftStart")) {
+                            shiftStart = punch.getAdjustedTimeStamp();
+                            //Gives Minutes
+                            shiftStart = (shiftStart / 1000) * 60;
                         }
+                        else if (punch.getAdjustedTimeStamp() == shiftTimes.get("lunchStop")) {
+                            lunchStop = punch.getAdjustedTimeStamp();
+                            //Gives Minutes
+                            lunchStop = (lunchStop / 1000) * 60;
+                        }
+                    // Time-Out
+                    case 2:
+                        pair = true;
                 }
             }
         }
 
-        int m = (int) (shift.getShiftDuration(shift.getStart(), shift.getStop()) -
-                shift.getLunchDuration(shift.getLunchStart(), shift.getLunchStop()));
+        System.out.println((lunchStart - shiftStart));
+        System.out.println((shiftStop - lunchStop));
+
+        int m = (int) ((lunchStart - shiftStart) + (shiftStop - lunchStop));
+
+        return m;
+    }*/
+
+    public static int calculateTotalMinutes(ArrayList<Punch> punches, Shift shift) {
+
+        boolean pair = false;
+        long in = 0;
+        long out = 0;
+        long beforeLunch = 0;
+        long afterLunch = 0;
+        int gotPunch = 0;
+
+        for (Punch punch : punches) {
+
+            HashMap<String, Long> shiftTimes = punch.getShiftTimes(shift);
+
+                switch(punch.getPunchtypeid()) {
+                    // Clock-Out
+                    case 0:
+                        out = punch.getAdjustedTimeStamp();
+                        //Gives Minutes
+                        out = (out / 1000) * 60;
+                        // Clock-In
+                    case 1:
+                        in = punch.getAdjustedTimeStamp();
+                        //Gives Minutes
+                        in = (in / 1000) * 60;
+                        // Time-Out
+                    case 2:
+                        gotPunch++;
+                }
+            }
+
+            if (beforeLunch == 0) {
+                beforeLunch = (out - in);
+            }
+            else if (afterLunch == 0) {
+                afterLunch = (out - in);
+            }
+
+        int m = (int) ((beforeLunch) + (afterLunch));
 
         return m;
     }
